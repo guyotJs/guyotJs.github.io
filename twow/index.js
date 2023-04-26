b5d2b5f9a2b72498e29f0f4c0b1fc5387e4bc489
Weston <classicmclive@gmail.com>
2023-04-26 15:46:14 -0400

    Update index.js

--- twow/index.js
+++ twow/index.js
@@ -1,6 +1,6 @@
 import { createApp } from 'https://unpkg.com/petite-vue?module'
 createApp({
-    version:"Pre-alpha 0.01", 
+    version:"Pre-alpha 0.1", 
     view:false,
     open(){
         this.view = true;
