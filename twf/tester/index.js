07db3852d9d0676bcb673fdec631fbae42d860ec
Weston <classicmclive@gmail.com>
2023-05-30 12:06:39 -0400

    Delete twf/tester directory

--- twf/tester/index.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'https://unpkg.com/petite-vue?module'
-function mobile(){
-  	if(window.innerWidth < window.innerHeight){return true;}
-    else{return false;}
-}
-createApp({
-	mobile
-}).mount()
